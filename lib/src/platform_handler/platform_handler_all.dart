ca56e67601ced5914677560258f6c4af6fde5195
dzham <dzham91@gmail.com>
2024-03-12 12:54:02 +0300

    Update platform_handler_all.dart
    update2

--- lib/src/platform_handler/platform_handler_all.dart
+++ lib/src/platform_handler/platform_handler_all.dart
@@ -67,6 +67,10 @@ class PlatformHandlerAll extends PlatformHandler {
     String? path;
     if (Platform.isAndroid || Platform.isIOS || Platform.isMacOS) {
       path = await _channel.invokeMethod<String>(_saveAs, fileModel.toMap());
+    }else if (Platform.isWindows) {
+      final Int64List? bytes =
+          await _channel.invokeMethod<Int64List?>('saveAs', fileModel.toMap());
+      path = bytes == null ? null : String.fromCharCodes(bytes);
     } else {
       throw UnimplementedError('Unimplemented Error');
     }
